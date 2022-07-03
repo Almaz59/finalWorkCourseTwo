01e2c22d29535ab60c88c3f47ce208172b120128
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-07-03 11:40:48 +0500

    finalWorkCourseTwo first commit

---
+++ src/main/java/pro/sky/finalworkcoursetwo/FinalWorkCourseTwoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.finalworkcoursetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalWorkCourseTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalWorkCourseTwoApplication.class, args);
+    }
+
+}
